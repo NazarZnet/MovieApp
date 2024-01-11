60f39194a1f42650f13236313d3e2f5bf91e1ff4
NazarZnet <nazarznetynyak123@gmail.com>
2024-01-11 23:54:12 +0200

    Made functions to get necessary data

--- app/api.py
+++ app/api.py
@@ -7,38 +7,78 @@ class MovieAPI:
             "Authorization": f"Bearer {api_key}"
         }
 
-    def get_now_playing(self, page:int=1)->dict:
+    def get_now_playing(self, page:int=1)->list:
         url = f"https://api.themoviedb.org/3/movie/now_playing?language=en-US&page={page}"
 
         response = requests.get(url, headers=self.headers)
         if response.status_code == 200:
             return response.json().get('results')
 
-        return {}
+        return []
     
-    def get_popular(self, page:int=1)->dict:
+    def get_popular(self, page:int=1)->list:
         url = f"https://api.themoviedb.org/3/movie/popular?language=en-US&page={page}"
 
         response = requests.get(url, headers=self.headers)
         if response.status_code == 200:
             return response.json().get('results')
 
-        return {}
+        return []
     
-    def get_top_rated(self, page:int=1)->dict:
+    def get_top_rated(self, page:int=1)->list:
         url = f"https://api.themoviedb.org/3/movie/top_rated?language=en-US&page={page}"
 
         response = requests.get(url, headers=self.headers)
         if response.status_code == 200:
             return response.json().get('results')
 
-        return {}
+        return []
 
-    def get_upcoming(self)->dict:
+    def get_upcoming(self)->list:
         url = f"https://api.themoviedb.org/3/movie/upcoming?language=en-US"
 
         response = requests.get(url, headers=self.headers)
         if response.status_code == 200:
             return response.json().get('results')
 
+        return []
+    
+    def get_movie_details(self, movie_id:int)->dict:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?language=en-US"
+
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 200:
+            return response.json()
+
         return {}
+    
+    def get_movie_images(self, movie_id:int)->list:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 200:
+            return response.json().get('backdrops')
+
+        return []
+    
+    def get_video_id(self, movie_id:int)->str:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}/videos?language=en-US"
+
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 200:
+            data = response.json().get('results')
+            for item in data:
+                if item.get('type') == 'Trailer' and item.get('official'):
+                    return item.get('key')
+
+        return None
+    
+    def get_similar_movies(self, movie_id:int)->list:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}/similar?language=en-US&page=1"
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 200:
+            data=response.json().get('results')
+            items=[item for item in data if item.get('popularity')>15]
+            return items
+
+        return []
