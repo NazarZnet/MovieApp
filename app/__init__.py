06f47437266753a89c5246e900aadbfc25579f9c
NazarZnet <nazarznetynyak123@gmail.com>
2024-01-11 18:17:17 +0200

    base main page frontend and got movies from api

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from config import Config
+from .api import MovieAPI
 
 def create_app():
     app = Flask(__name__, static_url_path='/static')
+
+    app.config.from_object(Config())
+
+    app.movieapi = MovieAPI(app.config['MOVIEAPI_KEY'])
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
