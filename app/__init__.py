f9b5c78189712e1655b0b17a5a3985baa0f9e7c7
NazarZnet <nazarznetynyak123@gmail.com>
2024-01-14 15:54:14 +0200

    Added base configiration for databse and JWT authorization

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,16 @@ from config import Config
 from .api import MovieAPI
 
 def create_app():
+    from .extensions import jwt, db
+
     app = Flask(__name__, static_url_path='/static')
 
     app.config.from_object(Config())
 
     app.movieapi = MovieAPI(app.config['MOVIEAPI_KEY'])
+
+    jwt.init_app(app)
+    db.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- config.py
+++ config.py
@@ -9,6 +9,11 @@ class Config:
         self.DEBUG = os.getenv('FLASK_DEBUG', False)
         self.HOST = os.getenv('FLASK_HOST', 'localhost')
         self.PORT = os.getenv('FLASK_RUN_PORT', 5000)
-        self.SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key')
-        self.MOVIEAPI_KEY = os.getenv('MOVIEAPI_KEY', 'your-movieapi-key')
-        # Add more configurations as needed
+        self.SECRET_KEY = os.getenv('SECRET_KEY', '11111')
+        self.MOVIEAPI_KEY = os.getenv('MOVIEAPI_KEY')
+
+        #authorizatio configurations
+        self.JWT_SECRET_KEY=os.getenv('JWT_SECRET_KEY')
+
+        #database configurations
+        self.SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
